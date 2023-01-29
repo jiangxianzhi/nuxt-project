136e3787f86413b99329ff2eea2561de20c472f7
Jiangxianzhi <772423266@qq.com>
2023-01-29 11:02:56 +0800

    my-nuxt-project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- template can only have one root element like div main -->
+<div>
+  <h1>hello world!</h1>
+<NuxtLink to="/">Home page</NuxtLink>
+<a href="https://nuxtjs.org">External Link to another page</a></div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
